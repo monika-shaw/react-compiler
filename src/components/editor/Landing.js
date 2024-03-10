6d9b304f50b9cdda7fbd7ba9bb5573fe71f7b323
shawmonika616ms <shawmonika616ms@gmail.com>
2024-03-10 09:55:08 +0530

    Description : 1. working on judge0 compiler. Developer : Monika.

--- src/components/editor/Landing.js
+++ src/components/editor/Landing.js
@@ -4,12 +4,77 @@ import CodeEditorWindow from './CodeEditorWindow'
 import LanguagesDropdown from './LanguagesDropdown'
 import ThemeDropdown from './ThemeDropdown';
 import { defineTheme } from '../../utils/defineTheme';
+import { languageOptions } from '../constants/languageOptions';
+import CustomInput from './CustomInput';
+import { classnames } from '../../utils/general';
+import OutputDetails from './OutputDetails';
+import OutputWindow from './OutputWindow';
+import useKeyPress from '../../hooks/useKeyPress';
+import axios from "axios";
+const javascriptDefault = `// some comment`;
 function Landing() {
+    const [code, setCode] = useState(javascriptDefault);
+    const [customInput, setCustomInput] = useState("");
+    const [outputDetails, setOutputDetails] = useState(null);
+    const [processing, setProcessing] = useState(null);
     const [theme, setTheme] = useState("cobalt");
+    const [language, setLanguage] = useState(languageOptions[0]);
     const onSelectChange = (sl) => {
         console.log("selected Option...", sl);
     };
 
+    const enterPress = useKeyPress("Enter");
+    const ctrlPress = useKeyPress("Control");
+    const handleCompile = () => {
+        setProcessing(true);
+        const formData = {
+            language_id: language.id,
+            // encode source code in base64
+            source_code: btoa(code),
+            stdin: btoa(customInput),
+        };
+        const options = {
+            method: "POST",
+            url: process.env.REACT_APP_RAPID_API_URL,
+            params: { base64_encoded: "true", fields: "*" },
+            headers: {
+                "content-type": "application/json",
+                "Content-Type": "application/json",
+                "X-RapidAPI-Host": process.env.REACT_APP_RAPID_API_HOST,
+                "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+            },
+            data: formData,
+        };
+        console.log(options.data);
+        axios
+            .request(options)
+            .then(function (response) {
+                console.log("res.data", response.data);
+                const token = response.data.token;
+                checkStatus(token);
+            })
+            .catch((err) => {
+                let error = err.response ? err.response.data : err;
+                setProcessing(false);
+                console.log(error);
+            });
+    };
+
+    const checkStatus = async (token) => {
+        // We will come to the implementation later in the code
+    };
+
+    const onChange = (action, data) => {
+        switch (action) {
+            case "code": {
+                setCode(data);
+                break;
+            }
+            default: {
+                console.warn("case not handled!", action, data);
+            }
+        }
+    };
     function handleThemeChange(th) {
         const theme = th;
         console.log("theme...", theme);
@@ -26,6 +91,14 @@ function Landing() {
             setTheme({ value: "oceanic-next", label: "Oceanic Next" })
         );
     }, []);
+
+    useEffect(() => {
+        if (enterPress && ctrlPress) {
+            console.log("enterPress", enterPress);
+            console.log("ctrlPress", ctrlPress);
+            handleCompile();
+        }
+    }, [ctrlPress, enterPress]);
     return (
         <>
             <ToastContainer
@@ -50,13 +123,13 @@ function Landing() {
             <div className="flex flex-row space-x-4 items-start px-4 py-4">
                 <div className="flex flex-col w-full h-full justify-start items-end">
                     <CodeEditorWindow
-                        code={"code"}
-                        onChange={"onChange"}
-                        language={"language?.value"}
+                        code={code}
+                        onChange={onChange}
+                        language={language?.value}
                         theme={theme.value}
                     />
                 </div>
-                {/* <div className="right-container flex flex-shrink-0 w-[30%] flex-col">
+                <div className="right-container flex flex-shrink-0 w-[30%] flex-col">
                     <OutputWindow outputDetails={outputDetails} />
                     <div className="flex flex-col items-end">
                         <CustomInput
@@ -75,7 +148,7 @@ function Landing() {
                         </button>
                     </div>
                     {outputDetails && <OutputDetails outputDetails={outputDetails} />}
-                </div> */}
+                </div>
             </div>
             {/* <Footer/> */}
         </>
